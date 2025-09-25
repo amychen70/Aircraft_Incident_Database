f75cc3546dfd8be3dd4d9c4228d14e2409187e02
amychen70 <amychen4399.work@gmail.com>
2025-09-24 22:37:39 -0400

    Create deleteDB.PractI.R

---
+++ deleteDB.PractI.R
@@ -0,0 +1,16 @@
+# Drop lookout table 
+status <- dbExecute(conn, "DROP TABLE IF EXISTS Incident;")
+status <- dbExecute(conn, "DROP TABLE IF EXISTS Flights;")
+
+# Drop lookout table 
+status <- dbExecute(conn, "DROP TABLE IF EXISTS incidentLookup")
+status <- dbExecute(conn, "DROP TABLE IF EXISTS severityLookup")
+status <- dbExecute(conn, "DROP TABLE IF EXISTS reportLookup")
+
+# Drop lookup table 
+status <- dbExecute(conn, "DROP TABLE IF EXISTS Airline")
+status <- dbExecute(conn, "DROP TABLE IF EXISTS Aircraft")
+status <- dbExecute(conn, "DROP TABLE IF EXISTS Dep_Airport")
+
+
+status <- dbDisconnect(conn)
